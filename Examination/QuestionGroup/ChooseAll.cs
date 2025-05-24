c1424af68348f82bac0964fcdc0905450a845a73
Steven Tharwat <stevven.helmy@gmail.com>
2025-05-24 22:37:59 +0300

    V1.1

--- Examination/QuestionGroup/ChooseAll.cs
+++
@@ -1,8 +0,0 @@
-﻿using Examination.AnswerGroup;
-
-namespace Examination.QuestionGroup
-{
-    internal class ChooseAll(string _Title, double _Marks, AnswerList list) : Question("Choose All Match Answer", _Title, _Marks, list)
-    {
-    }
-}
